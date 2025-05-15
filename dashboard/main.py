d3d99124293549a7d32c31ca000d5237aa4d10b8
gzangalli <gustavo.vzangalli@gmail.com>
2025-05-15 18:38:46 -0300

    Melhoria no streamlit para poder escolher a moeda a acompanhar

--- dashboard/main.py
+++ dashboard/main.py
@@ -9,6 +9,7 @@ load_dotenv('../')
 
 DATABASE_URL = os.getenv("DATABASE_KEY")
 
+
 def ler_dados_postgres():
     try:
         conn = psycopg2.connect(DATABASE_URL)
@@ -27,30 +28,40 @@ def ler_dados_postgres():
         st.error(f"Erro ao conectar no PostgreSQL: {e}")
         return pd.DataFrame()
 
+
 def main():
-    st.set_page_config(page_title="Dashboard de Preços do Bitcoin", layout="wide")
+    st.set_page_config(
+        page_title="Dashboard de Preços do Bitcoin", layout="wide")
     st.title("📊 Dashboard de Preços do Bitcoin")
     st.write("Este dashboard exibe os dados do preço do Bitcoin coletados periodicamente em um banco PostgreSQL.")
 
     df = ler_dados_postgres()
 
     if not df.empty:
-        st.subheader("📋 Dados Recentes")
-        st.dataframe(df)
-
         df['timestamp'] = pd.to_datetime(df['timestamp'])
         df = df.sort_values(by='timestamp')
-        
-        st.subheader("📈 Evolução do Preço do Bitcoin")
-        st.line_chart(data=df, x='timestamp', y='valor', use_container_width=True)
 
-        st.subheader("🔢 Estatísticas Gerais")
+        moedas = df['criptomoeda'].unique()
+        criptomoeda_selecionada = st.selectbox(
+            "🔍 Selecione a Criptomoeda:", moedas)
+
+        df_filtrado = df[df['criptomoeda'] == criptomoeda_selecionada]
+
+        st.subheader("📋 Dados Recentes")
+        st.dataframe(df_filtrado, use_container_width=True)
+
+        st.subheader("📈 Evolução do Preço")
+        st.line_chart(data=df_filtrado, x='timestamp',
+                      y='valor', use_container_width=True)
+
+        st.subheader("📊 Estatísticas")
         col1, col2, col3 = st.columns(3)
-        col1.metric("Preço Atual", f"${df['valor'].iloc[-1]:,.2f}")
-        col2.metric("Preço Máximo", f"${df['valor'].max():,.2f}")
-        col3.metric("Preço Mínimo", f"${df['valor'].min():,.2f}")
+        col1.metric("💵 Preço Atual", f"${df_filtrado['valor'].iloc[-1]:,.2f}")
+        col2.metric("📈 Preço Máximo", f"${df_filtrado['valor'].max():,.2f}")
+        col3.metric("📉 Preço Mínimo", f"${df_filtrado['valor'].min():,.2f}")
     else:
-        st.warning("Nenhum dado encontrado no banco de dados PostgreSQL.")
+        st.warning("⚠️ Nenhum dado encontrado no banco de dados PostgreSQL.")
+
 
 if __name__ == "__main__":
     main()
